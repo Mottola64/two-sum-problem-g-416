5892460ef8d629b14b288bd8ab654f955712c104
Michael Mottola <mottola64@gmail.com>
2020-09-24 01:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function bruteForceTwoSum(sum, a) {
-  
+function bruteForceTwoSum(array, sum) {
+  let pairs = [];
+
+  while (array.length != 0){
+    let currNum = array.shift()
+
+    for (let i=0; i < array.length; i++){
+      if (array[i] + currNum === sum){
+        pairs.push([currNum, array[i]])
+      }
+    }
+  }
+
+  return pairs
 }
